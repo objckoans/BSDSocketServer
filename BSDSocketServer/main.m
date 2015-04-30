df8d26ca9d4be2ca379af5b5862d98ed74c365da
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-30 12:12:25 +0800

    Using our BSDSocketServer to instantiate a server

--- BSDSocketServer/main.m
+++ BSDSocketServer/main.m
@@ -7,11 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BSDSocketServer.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        BSDSocketServer *bsdServ = [[BSDSocketServer alloc] initOnPort:8888];
+        if (bsdServ.errorCode == NOERROR) {
+            [bsdServ echoServerListenWithDescriptor:bsdServ.listenfd];
+        } else {
+            NSLog(@"%@", [NSString stringWithFormat:@"Error code %d received. Server was not started", bsdServ.errorCode]);
+        }
+
     }
     return 0;
 }
